43a7c521d8a1ea67803504faadf58e33f7af2615
joona374 <Joona.Rahm@student.lab.fi>
2025-10-03 00:57:56 +0300

    update server

--- final_coffee_pipeline/backend/server.js
+++ final_coffee_pipeline/backend/server.js
@@ -1,35 +1,15 @@
 import express from "express";
-import { WebSocketServer } from "ws";
-
 const app = express();
-const PORT = 3000;
 
-// Basic REST for testing
+app.get("/should_start", (req, res) => {
+  res.set("Connection", "close"); // important for Pico
+  res.set("Transfer-Encoding", "identity"); // disable chunked
+  res.json({ brew: true });
+});
+
 app.get("/", (req, res) => {
-  res.send("Coffee backend running ☕");
+  res.send("Coffee backend is alive");
 });
 
-const server = app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
-// BASIC REST FOR TESTING
-
-const wss = new WebSocketServer({ server });
-
-wss.on("connection", (socket) => {
-  console.log("Pico connected via socket");
-
-  socket.on("message", (data) => {
-    console.log("Received data from Pico:", data.toString());
-    // Echo the data back to the client
-    socket.send(`Server received: ${data}`);
-  });
-
-  socket.on("close", () => {
-    console.log("Pico disconnected");
-  });
-
-  setInterval(() => {
-    socket.send(JSON.stringify({ action: "brew" }));
-  }, 10000);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on", PORT));

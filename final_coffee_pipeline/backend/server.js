f28775d8d8bdc03b6e4ebb6d94f9a6c4a95267ed
joona374 <Joona.Rahm@student.lab.fi>
2025-10-05 17:17:56 +0300

    Added skeleton express for render to have a http port

--- final_coffee_pipeline/backend/server.js
+++ final_coffee_pipeline/backend/server.js
@@ -4,6 +4,18 @@ const broker = "mqtt://test.mosquitto.org:1883";
 const topicSub = "iot/demo/responses";
 const topicPub = "iot/demo/commands"; // the topic your Pico subscribes to
 
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
 // Connect to the broker
 const client = mqtt.connect(broker);
 
